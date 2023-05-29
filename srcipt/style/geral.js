b72892de5535fcced38df77bdb0055b5d3f1e8bd
RayAra2004 <raynan.araujo@gmail.com>
2023-05-29 12:03:48 -0300

    Bootstrap - Home

--- srcipt/style/geral.js
+++ srcipt/style/geral.js
@@ -1,20 +1,20 @@
 function header(){
     const header = document.getElementById("cabecalho").innerHTML = `
-    <nav class="navbar navbar-expand-sm bg-body-tertiary" data-bs-theme="dark">
+    <nav class="navbar navbar-expand-sm bg-body-tertiary m-2" data-bs-theme="dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="./../index.html"><img src="../imgs/logo_recicla_mais.svg" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a href="../index.html" class="nav-link">Home</a>
-                <a href="../telas/sobreNos.html" class="nav-link">Sobre Nós</a>
-                <a href="" class="nav-link">Seja um validador</a>
-                <a href="../telas/ajuda.html" class="nav-link">Ajuda</a>
+                <div class="navbar-nav">
+                    <a href="../index.html" class="nav-link">Home</a>
+                    <a href="../telas/sobreNos.html" class="nav-link">Sobre Nós</a>
+                    <a href="" class="nav-link">Seja um validador</a>
+                    <a href="../telas/ajuda.html" class="nav-link">Ajuda</a>
+                </div>
             </div>
-            </div>
-            </div>
+        </div>
     </nav>
     `    
     let a = document.getElementsByClassName("nav-link");
