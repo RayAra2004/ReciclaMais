ec8d60209c6df73346bcb9e85e4fa4964053db32
Kaua <kaua271203@gmail.com>
2023-06-12 12:16:03 -0300

    update header entrar button

--- srcipt/cabecalho/geral.js
+++ srcipt/cabecalho/geral.js
@@ -6,12 +6,13 @@ function header(){
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="collapse navbar-collapse justify-content-md-end" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a href="../index.html" class="nav-link">Home</a>
                     <a href="../telas/sobreNos.html" class="nav-link">Sobre Nós</a>
                     <a href="" class="nav-link">Seja um validador</a>
                     <a href="../telas/ajuda.html" class="nav-link">Ajuda</a>
+                    <a class="btn" href="#" role="button">Entrar</a>
                 </div>
             </div>
         </div>
@@ -20,5 +21,10 @@ function header(){
     let a = document.getElementsByClassName("nav-link");
     for (i = 0; i < a.length; i++) {
         a[i].style.color = "#05e981";
+    }
+    let b = document.getElementsByClassName("btn");
+    for (i = 0; i < b.length; i++) {
+        b[i].style.color = "#000";
+        b[i].style.background = "#05e981";
     }
 }
