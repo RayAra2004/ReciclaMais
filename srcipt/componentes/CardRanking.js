d7304431b1a16d46f82a35b2b38ce5c2b71c65b9
RayAra2004 <raynan.araujo@gmail.com>
2023-06-19 12:56:02 -0300

    tela ranking

---
+++ srcipt/componentes/CardRanking.js
@@ -0,0 +1,34 @@
+const ranking1 = document.getElementById('ranking1');
+const ranking2 = document.getElementById('ranking2');
+
+let rankig = '';
+for(let i = 0; i < 10; i++){
+    rankig += `
+    <div class="card mb-3 ranking-empresa">
+        <div class="row g-0">
+            <div class="col-md-4 img-empresa">
+                <img src="./../imgs/logoMA.png" class="img-empresa img-fluid rounded-start">
+            </div>
+            <div class="col-md-8">
+                <div class="card-header">
+                    <h5 class="card-title">MARCA AMBIENTAL</h5>
+                </div>
+                <div class="card-body d-flex mt-3 infos-card">
+                    <p class="card-text d-flex">
+                        <ion-icon class="" name="pint-outline"></ion-icon>
+                        <ion-icon class="ms-3" name="star"></ion-icon>
+                        <span>4.8/5</span>
+                        <button class="btn-infos ms-3">Ver mais</button>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    `
+}
+
+ranking1.innerHTML = rankig;
+ranking2.innerHTML = rankig;
+
+            
