32962f09bd67e7d374339ecfe07f8cdddfc4f3e7
Kaua <kaua271203@gmail.com>
2023-06-19 12:34:21 -0300

    pequenos ajustes index comoReciclar geral

--- srcipt/cabecalho/geral.js
+++ srcipt/cabecalho/geral.js
@@ -1,8 +1,9 @@
 function header(){
-    const header = document.getElementById("cabecalho").innerHTML = `
+    const header = document.getElementById("cabecalho").innerHTML = 
+    `
     <nav class="navbar navbar-expand-sm bg-body-tertiary" data-bs-theme="dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="./../index.html"><img src="../imgs/logo_recicla_mais.svg" alt=""></a>
+            <a class="navbar-brand" href="./../index.html"><img src="../imgs/logo_recicla_mais.svg" class="logo-top" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
@@ -29,4 +30,17 @@ function header(){
     }
 }
 
-//po
+function footer(){
+    const footer = document.getElementById("rodape").innerHTML =
+    `   
+    <footer class="d-flex flex-wrap justify-content-around align-items-center py-3 my-4 border-top">
+        <div class="d-flex align-items-center">
+        <a href="#" class="">
+            <img src="./imgs/logo_recicla_mais.svg" alt="" class="logo-bottom">
+        </a>
+        <p class="mb-3 mb-md-0  temaGreen">reciclamaissuporte@gmail.com</p>
+        </div>
+    </footer>
+    <p class="text-center pb-5  temaGreen">Reciclar nunca foi tão fácil!</p>
+    `
+}
