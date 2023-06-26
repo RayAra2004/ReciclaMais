e47499025aaabc5a69ec7b7f9916838f7181b216
Kaua <kaua271203@gmail.com>
2023-06-26 12:47:23 -0300

    Geral ajustado

--- srcipt/cabecalho/geral.js
+++ srcipt/cabecalho/geral.js
@@ -33,11 +33,11 @@ function footer(){
     const footer = document.getElementById("rodape").innerHTML =
     `   
     <footer class="d-flex flex-wrap justify-content-around align-items-center py-3 my-4 border-top">
-        <div class="d-flex align-items-center flex-wrap">
-        <a href="#" class="">
-            <img src="./../imgs/logo_recicla_mais.svg" alt="" class="logo-bottom">
-        </a>
-        <p class="mb-3 mb-md-0  temaGreen">reciclamaissuporte@gmail.com</p>
+        <div class="d-flex align-items-center">
+            <a href="#" class="">
+                <img src="./../imgs/logo_recicla_mais.svg" alt="" class="logo-bottom">
+            </a>
+            <p class="mb-3 mb-md-0  temaGreen">reciclamaissuporte@gmail.com</p>
         </div>
     </footer>
     <p class="text-center pb-4  temaGreen">Reciclar nunca foi tão fácil!</p>
