cf0eab795dae896877cc32f870c09d172fb32fce
RayAra2004 <raynan.araujo@gmail.com>
2023-08-11 11:00:39 -0300

    Pagina Inicial Empresa: Incompleta

---
+++ telas/paginaInicialEmpresa.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recicla +</title>
+    <link rel="stylesheet" href="../css/reset.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">  </head>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous" defer></script>
+    
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+
+    <script src="../srcipt/cabecalho/geral.js" defer></script>
+
+    <link rel="stylesheet" href="./../css\paginaInicialEmpresa.css">
+    
+</head>
+<body onload="header(); footer()">
+    <header id="cabecalho"></header>
+
+    <div class="conteudo">
+        <div class="opcoes">
+            <div class="grupo">
+                <div class="option">
+                    <span class="material-icons">
+                        location_on
+                    </span>
+                    <p>Pontos De Coleta</p>
+                </div>
+            </div>
+            <div class="grupo">
+                <br>
+                <br>
+                <div class="option">
+                    <span class="material-icons">
+                        add_circle_outline
+                    </span>
+                    <p>Adicionar Ponto</p>
+                </div>
+            </div>
+        </div>
+    
+        <div class="opcoes">
+            <div class="grupo">
+                <div class="option">
+                    <span class="material-icons">
+                        recycling
+                    </span>
+                    <p>Reciclagem de Materiais</p>
+                </div>
+            </div>
+            <div class="grupo">
+                <div class="option">
+                    <span class="material-icons">
+                        stars
+                    </span>
+                    <p>Ranking</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="opcoes">
+            <div class="grupo">
+                <div class="option">
+                    <span class="material-symbols-outlined">
+                        photo_camera
+                    </span>
+                    <p>Postagem de Materiais</p>
+                </div>
+            </div>
+            <div class="grupo">
+                <div class="option">
+                    <span class="material-icons">
+                        stars
+                    </span>
+                    <p>Suas negociações</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div id="rodape" class="container-fluid"></div>
+</body>
+</html>
