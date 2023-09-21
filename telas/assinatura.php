ffff6c1a5b140284630d8d557dce4d63b2aab4d7
20201TIIMI0128 <kaua271203@gmail.com>
2023-09-21 11:47:55 -0300

    Assinatura antiga pronta

--- telas/assinatura.php
+++ telas/assinatura.php
@@ -19,37 +19,55 @@
 <body>
     <div class="container mt-5 todo">
         <div class="container d-flex justify-content-center flex-wrap">
-            <div class="container-fluid m-0 p-0 div-img-login">
+            <!--<div class="container-fluid m-0 p-0 div-img-login">
                 <img class="img-fluid img-login" src="./../imgs/login_desktop.png"/>
-            </div>
-            <div class="col-lg-4 div-form">
-                <div class="container card px-5 py-3 form1">
-                <form action="/cadastro" method="post">
-                    <div class="mb-3">
-                    <label for="numero_cartao" class="form-label">Número do cartão</label>
-                    <input type="text" class="form-control" id="numero_cartao" name="numero_cartao" style="width: 100%;">
-                    </div>
-                    <div class="mb-3">
-                    <label for="nome_titular" class="form-label">Nome do titular</label>
-                    <input type="text" class="form-control" id="nome_titular" name="nome_titular" style="width: 100%;">
-                    </div>
-                    <div class="mb-3">
-                    <label for="validade" class="form-label">Validade</label>
-                    <input type="month" class="form-control" id="validade" name="validade" style="width: 100%;">
-                    </div>
-                    <div class="mb-3">
-                    <label for="cvv" class="form-label">CVV</label>
-                    <input type="text" class="form-control" id="cvv" name="cvv" style="width: 100%;">
-                    </div>
-                    <div class="mb-3">
-                    <label for="bandeira" class="form-label">Tipo</label>
-                    <select class="form-control preto" id="bandeira" name="bandeira">
-                        <option value="visa" class="preto">Crédito</option>
-                        <option value="mastercard" class="preto">Débito</option>
-                    </select>
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-cadastre" id="finaliza_cartão">Cadastrar</button>
-                </form>
+            </div>-->
+                <div class="container card px-5 py-5 form1">
+                    <form action="/cadastro" method="post">
+                        <div class="d-flex flex-wrap justify-content-evenly">
+                            <div class="fr">
+                                <div class="mb-3">
+                                    <label for="numero_cartao" class="form-label">Número do cartão</label>
+                                    <input type="text" class="form-control" id="numero_cartao" name="numero_cartao" style="width: 100%;">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="validade" class="form-label">Data de Vencimento</label>
+                                    <input type="month" class="form-control" id="validade" name="validade" style="width: 100%;">
+                                </div>
+
+                                <div class="mb-3">
+                                    <label for="nome_titular" class="form-label">CPF</label>
+                                    <input type="text" class="form-control" id="cpf" name="cpf" style="width: 100%;">
+                                </div>
+                            </div>
+
+                            <div class="fr">
+                                <div class="mb-3">
+                                    <label for="nome_titular" class="form-label">Nome do titular</label>
+                                    <input type="text" class="form-control" id="nome_titular" name="nome_titular" style="width: 100%;">
+                                </div>
+                            
+                                <div class="mb-3">
+                                    <label for="cvv" class="form-label">CVV</label>
+                                    <input type="text" class="form-control" id="cvv" name="cvv" style="width: 100%;">
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="credito" id="credito" checked>
+                                    <label class="form-check-label mt-1" for="credito">
+                                        Crédito
+                                    </label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="debito" id="debito">
+                                        <label class="form-check-label mt-1" for="debito">
+                                            Débito
+                                        </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-center"><button type="submit" class="btn btn-primary btn-cadastre " id="finaliza_cartão">Assinar</button></div>
+                    </form>
                 </div>
                 <div class="card px-5 py-3 h-50 d-none justify-content-center align-content-center flex-wrap invalid-email" id="invalid-email">
                     <div class="container-progress">
@@ -63,7 +81,7 @@
                     </div>
                     <p class="text-center">SENHA INCORRETA</p>
                 </div>
-            </div>
+            
         </div>
     </div>
 </body>
