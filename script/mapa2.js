46158a16b8f8b49e76f6c63c7bbca542024d0801
Davi <sallescardosodavi@gmail.com>
2023-10-20 17:15:55 -0300

    mapa sem erros com um ponto e centralizado no ifes

--- script/mapa2.js
+++ script/mapa2.js
@@ -1,16 +1,85 @@
+"use strict"
+const searchInput = document.querySelector(".search_input");
+const searchBtn = document.querySelector(".search_btn");
 
-var map = null;
+let map, searchManager;
+
+searchBtn.addEventListener("click", ()=>{
+    map.entities.clear();
+    geocodeQuery(searchInput.value);
+});
+
+function getMap(){
+    var locIfes = new Microsoft.Maps.Location(-20.197329691804068, -40.2170160437478);
+    var jaymeDosSantosNeves = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-20.199232504534884, -40.227077110956316), {
+        color: "red",
+        title: "Hospital Jayme dos Santos Neves",
+        icon: "/ReciclaMais/imgs/icone-ponto-prata.png"
+    });
+
+    /*
+    var locaisProprios = {};
+    
+
+    var jaymeDosSantosNeves = new Microsoft.Maps.Pushpin(new Microsoft.Maps.Location(-20.199232504534884, -40.227077110956316), {
+        color: "red",
+        title: "Hospital Jayme dos Santos Neves",
+        icon: "../imgs/icone-ponto-prata.png"
+    });
+
+    for (let item in locaisProprios) {
+        map.entities.push(locaisProprios[item]["pushpin"]);
+    }
+    */
+    map = new Microsoft.Maps.Map('#map', {
+        // You need your key.
+        center: locIfes,
+        zoom: 16,
+        NavigationBarMode: "minified",
+        navigationBarOrientation: "horizontal",
+        credentials: 'Ak_PJnVWlG661PnFrGXTK6jXXiZz9b3Ocn4R5X9BXIhfGRcR7zSvm27_YE30YHHK',
+    });
+    /*for (let item in locaisProprios) {
+        map.entities.push(locaisProprios[item]["pushpin"]);
+    }*/
+    map.entities.push(jaymeDosSantosNeves);
+};
+
+function geocodeQuery(query){
+    if (!searchManager) {
+        Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+            searchManager = new Microsoft.Maps.Search.SearchManager(map);
+            geocodeQuery(query);
+        });
+    } else {
+        let searchRequest = {
+            where: query,
+            callback: function (r) {
+                if (r && r.results && r.results.length > 0) {
+                    var pin = new Microsoft.Maps.Pushpin(r.results[0].location);
+                    map.entities.push(pin);
+
+                    map.setView({ bounds: r.results[0].bestView });
+                };
+            },
+            errorCallback: function (e) {
+                alert("No results found.");
+            }
+        };
+        searchManager.geocode(searchRequest);
+    };
+};
 
 
-
-/*esta funcao carrega o mapa*/
-function loadMapScenario() {
-    const mapa = document.getElementById("mapa");
-    /*cria um objeto de mapa da microsoft e adiciona a div que ira conter o mapa*/
-    map = new Microsoft.Maps.Map(document.getElementById("mapa"));
-}
-
-//funcao que roda quando o usuario clica enter no input sem escolher uma sugestao
-
-
-
+/*Microsoft.Maps.Events.addHandler(map, 'viewchangeend', function (e) {
+    if (map.getZoom() < 16) {
+        for (let item in locaisProprios) {
+            map.entities.pop(locaisProprios[item]["pushpin"]);
+        }
+    }
+    else {
+        for (let item in locaisProprios) {
+            map.entities.push(locaisProprios[item]["pushpin"]);
+        }
+    }
+});*/
--- telas/mapa.php
+++ telas/mapa.php
@@ -1,55 +1,16 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Mapa</title>
-
-  <!--importando style da página-->
-
-  <!--importando style do icones do bootstrap-->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
-
-  <!--importando style do fontawesome-->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-  <!--importando script da página-->
-
-  <!--importando script dos mapas do bing-->
-  <script type='text/javascript'
-    src='https://www.bing.com/api/maps/mapcontrol?callback=loadMapScenario&key=Agz-GsinzRU8zLEoIGspfeW14MkrCmOv1RXL5foc3GtKtQWkGHydai2rkhG_ZwQu&libs=Microsoft.Maps.Search'></script>
-
-  <!--importando style do bootstrap-->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-
-  <!--importando script do bootstrap-->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"
-    defer></script> 
-</head>
-
-<body>
-  <?php
-    $css = '<link rel="stylesheet" href="/ReciclaMais/css/mapa.css"> <script src="/ReciclaMais/script/mapa2.js" defer></script>';
-    include './componentes/header.php';
-  ?>
-
-  <!--section onde o mapa e o menu de pesquisa lateral ficara posicionado-->
-  <section id="containerMapa">
-    <!--div onde o mapa sera renderizado-->
-    <div id="mapa" style="height: 50vh; width:80vh"></div>
-    <!--Fim da section-->
-  </section>
-
-
-
-
-
-</body>
-
-</html>
+<?php
+  $css = '<link rel="stylesheet" href="/ReciclaMais/css/mapa.css">
+    <script src="/ReciclaMais/script/mapa2.js" defer></script>
+    <script src="http://www.bing.com/api/maps/mapcontrol?callback=getMap" async></script>';
+  include './componentes/header.php';
+?>
+<section id="containerMapa">
+  <div class="options">
+      <input class="search_input" placeholder="Search">
+      <button class="search_btn">Search</button>
+  </div>
+  <div id="map"></div>
+</section>
+<?php
+    include './componentes/footer.php'
+?>    
