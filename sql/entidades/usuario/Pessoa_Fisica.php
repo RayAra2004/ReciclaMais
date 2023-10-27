266cb3b4ae191d95f026326ac4712ff7294c95b9
RayAra2004 <raynan.araujo@gmail.com>
2023-10-26 23:34:59 -0300

    fix: correção da query sql

--- sql/entidades/usuario/Pessoa_Fisica.php
+++ sql/entidades/usuario/Pessoa_Fisica.php
@@ -14,7 +14,7 @@
             //chama o insert de Usuario
             if (parent::insert()) {
                 // Agora, vamos inserir os dados específicos de pessoa física (data de nascimento).
-                $sql = "INSERT INTO $this->table (id, data_nascimento) VALUES (:id, :dataNascimento);";
+                $sql = "INSERT INTO $this->table (fk_usuario_id, data_nascimento) VALUES (:id, :dataNascimento);";
                 $stmt = Database::prepare($sql);
                 $stmt->bindParam(':id', $this->getId(), PDO::PARAM_INT); // Obtém o ID do usuário
                 $stmt->bindParam(':dataNascimento', $this->dataNascimento);
