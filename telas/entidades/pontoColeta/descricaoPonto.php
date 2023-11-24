c411628b383333a1e22cb3c10b078ad2b1ab9ffb
RayAra2004 <raynan.araujo@gmail.com>
2023-11-24 14:43:34 -0300

    descrição do ponto --> sem comentários e nota

--- telas/entidades/pontoColeta/descricaoPonto.php
+++ telas/entidades/pontoColeta/descricaoPonto.php
@@ -1,6 +1,28 @@
 <?php 
     $css = '<link rel="stylesheet" href="/ReciclaMais/css/descricaoPonto.css"> <script src="./../script/componentes/Comentario.js" defer></script>';
     include './../../componentes/header.php';
+    include './../../../sql/entidades/pontoColeta/PontoColeta.php';
+    include './../../../sql/entidades/endereco/Endereco.php';
+    include './../../../sql/entidades/usuario/Usuario.php';
+
+    $id_pontoColeta = $_GET['id'];
+
+    $newPontoColeta = new PontoColeta();
+    $pontoColeta = $newPontoColeta->find($id_pontoColeta);
+
+    $newUsuario = new Usuario();
+    $usuario = $newUsuario->find($pontoColeta["fk_usuario_id"]);
+
+    $endereco = Endereco::getDados($pontoColeta["fk_endereco_id"]);
+    $tipo_logradouro = ucfirst($endereco["tipo_logradouro"]);
+    $cep = substr_replace($endereco["cep"], '-', -3, 0);
+    $telefone = substr_replace($usuario["telefone"], '(', 0, 0);
+    $telefone = substr_replace($telefone, ') ', 3, 0);
+    if(strlen($usuario["telefone"]) == 11){
+        $telefone = substr_replace($telefone, '-', 10, 0);
+    }else{
+        $telefone = substr_replace($telefone, '-', 9, 0);
+    }
 ?>
 <section class="body_content">
     <div class="container d-flex flex-wrap flex-column align-content-center pt-4">
@@ -14,32 +36,24 @@
             </div>
         </div>
         <div class="container d-flex justify-content-center">
-            <img class="img-fluid w-25" src="./../imgs/pessoas_reciclando.jpg" alt="">
+            <img class="img-fluid w-25" src="<?php echo $pontoColeta["imagem"];?>" alt="">
         </div>
         <div class="container text-center mt-3 pt-5 infos-gerais">
-            <p>NOME: MARCA AMBIENTAL</p>
+            <p>NOME: <?php echo $pontoColeta["nome"];?></p>
             <p>ENDEREÇO:</p>
-            <p>Rod. Gov. Mário Covas, 1864 - Padre Martins, Cariacica - ES, <br> 29157-100</p>
+            <p><?php echo $tipo_logradouro;?> <?php echo $endereco["logradouro"];?>, 
+                <?php echo $endereco["numero"];?> - <?php echo $endereco["bairro"];?>, 
+                <?php echo $endereco["cidade"];?> - <?php echo $endereco["estado"];?>,
+                <br> <?php echo $cep;?>
+            </p>
             <br> <br>
-            <p>TELEFONE: (27) 2123-7700</p>
-        </div>
-        <div class="container mt-3 d-flex flex-column flex-wrap justify-content-center align-content-center avaliacao">
-            <p class="mb-4 text-center">AVALIE O PONTO</p>
-            <div class="d-flex justify-content-center">
-                <ion-icon class="estrela" name="star-outline"></ion-icon>
-                <ion-icon class="estrela" name="star-outline"></ion-icon>
-                <ion-icon class="estrela" name="star-outline"></ion-icon>
-                <ion-icon class="estrela" name="star-outline"></ion-icon>
-                <ion-icon class="estrela" name="star-outline"></ion-icon>
-            </div>
-        </div>
-        <div class="container comentario d-flex flex-column flex-wrap align-content-center mt-3">
-            <p class="mb-3">ADICIONE UM COMENTÁRIO</p>
-            <input type="text" name="" id="" placeholder="Adicione um comentário">
+            <p>TELEFONE: <?php echo $telefone;?></p>
         </div>
         <div class="container mt-4 comentarios"></div>
         <div class="container mt-5 mb-5 abrir-maps d-flex justify-content-center">
-            <button>ABRIR TRAGETÓRIA ATÉ O PONTO</button>
+            <a href="https://www.google.com/maps/place/<?php echo $endereco["latitude"];?>, <?php echo $endereco["longitude"];?>" target="_blank">
+                <button>TRAGETÓRIA ATÉ O PONTO</button>
+            </a>
         </div>
     </div>
 </section>
