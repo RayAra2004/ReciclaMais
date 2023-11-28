6bcd385253d01b1ff959145adc204fe034e8fa0e
20201TIIMI0128 <kaua271203@gmail.com>
2023-11-28 09:14:27 -0300

    add btn filter and input inside map

--- telas/entidades/pontoColeta/pontosColeta.php
+++ telas/entidades/pontoColeta/pontosColeta.php
@@ -1,47 +1,56 @@
 <?php
-    $css = '<script src="/ReciclaMais/script/map.js" defer></script><link rel="stylesheet" href="/ReciclaMais/css/pontosColeta.css"><link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="">';
-    include './../../componentes/header.php';
+  $css = '<link rel="stylesheet" href="/ReciclaMais/css/mapa.css">
+  <script src="/ReciclaMais/script/mapa2.js" defer></script>
+  <script src="http://www.bing.com/api/maps/mapcontrol?callback=getMap" async></script>';
+
+  include './../../componentes/header.php';
+  include './../../../sql/entidades/pontoColeta/PontoColeta.php';
+  
+  if (!(isset($_SESSION['comp_header']))){
+    $_SESSION['comp_header'] = '<a class="btn temaGreen" href="/ReciclaMais/telas/entidades/login/login.php" role="button">Entrar</a>';
+  }
+
+  $dicionario = [];
+
+  // Adicionando o primeiro item ao dicionário
+  $dicionario["-20.197329691804068, -40.2170160437478"] = [
+      "title" => "Ifes Campus Serra",
+      "icon" => "/ReciclaMais/imgs/silver_pin.svg"
+  ];
+
+  // Adicionando o segundo item ao dicionário
+  $dicionario["-20.199232504534884, -40.227077110956316"] = [
+      "title" => "Hospital Jayme dos Santos Neves",
+      "icon" => "/ReciclaMais/imgs/silver_pin.svg"
+  ];
+
+  // Adicionando o terceiro item ao dicionário
+  $dicionario["-20.19826402415827, -40.224856532079116"] = [
+      "title" => "Café Arrumado",
+      "icon" => "/ReciclaMais/imgs/silver_pin.svg"
+  ];
+  echo "<div id='listPntos'>";
+  echo json_encode($dicionario);
+  echo "</div>";
+  
+  
 ?>
-    <section class="body_content">
-    <div class="container d-flex info">
-        <ion-icon name="filter-outline"></ion-icon>
-        <p class="ms-2">FILTRO DOS PONTOS</p>
+<section id="containerMapa">
+  <!--<div id="divtot">-->
+    <div id="divzada">
+      <button id="btnClose">X</button>
+      <img id="imgPonto" src="/ReciclaMais/imgs/arvores_home.jpg" alt="">
+      <p id="text1"></p>
     </div>
-    <div class="row m-0 p-0">
-        <div class="container ms-3 me-3 mt-3 checks col-4">
-            <p>MATERIAIS QUE VOCÊ IRÁ DESCARTAR</p>
-            <div class="container mt-2">
-                <form action="">
-                    <fieldset>
-                        <input type="checkbox" name="" id="" value="VIDRO"><span>VIDRO</span><br>
-                        <input type="checkbox" name="" id="" value="PLÁSTICO"><span>PLÁSTICO</span><br>
-                        <input type="checkbox" name="" id="" value="MADEIRA"><span>MADEIRA</span><br>
-                        <input type="checkbox" name="" id="" value="ELETRÔNICO"><span>ELETRÔNICO</span><br>
-                        <input type="checkbox" name="" id="" value="METAIS"><span>METAIS</span><br>
-                        <input type="checkbox" name="" id="" value="PAPEL"><span>PAPEL</span><br>
-                        <input type="checkbox" name="" id="" value="ORGÂNICO"><span>ORGÂNICO</span><br>
-                        <input type="checkbox" name="" id="" value="HOSPITALAR"><span>HOSPITALAR</span><br>
-                    </fieldset>
-                </form>
-            </div>
-            <p class="mt-3">DISTÂNCIA DO PONTO DE COLETA</p>
-            <div class="container mt-2">
-                <form action="">
-                    <fieldset>
-                        <input type="checkbox" name="" id="" value="3"><span>até 3KM</span><br>
-                        <input type="checkbox" name="" id="" value="7"><span>até 7KM</span><br>
-                        <input type="checkbox" name="" id="" value="12"><span>até 12KM</span><br>
-                        <input type="checkbox" name="" id="" value="+12"><span>+12KM</span><br>
-                    </fieldset>
-                </form>
-            </div>
-        </div>
-        <div class="container col-5 maps">
-            <div id="map"></div>
-        </div>
-    </div>
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+  <!--</div>-->
+  <div class="options">
+      <div id="closedFilter">Filtros</div>
+      <div id="openedFilter">Filtros</div>
+      <input class="search_input" placeholder="Search">
+      <button class="search_btn">Search</button>
+  </div>
+  <div id="map"></div>
 </section>
 <?php
-    include './../../componentes/footer.php'
-?>
+    include '../../componentes/footer.php'
+?>    
