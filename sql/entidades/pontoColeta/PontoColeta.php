4bc7aafe57015d0db8f53e9c5535f472591eecd7
RayAra2004 <raynan.araujo@gmail.com>
2023-12-06 21:50:19 -0300

    detalhes pontos

--- sql/entidades/pontoColeta/PontoColeta.php
+++ sql/entidades/pontoColeta/PontoColeta.php
@@ -105,6 +105,33 @@
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
 
+        public static function findPontoColetaById($id){
+            $sql = "SELECT cadastro_ponto_coleta.nome, cadastro_ponto_coleta.id, cadastro_ponto_coleta.imagem,
+                endereco.cep, endereco.latitude, endereco.longitude, endereco.logradouro, endereco.numero,
+                endereco.complemento, estado.estado, cidade.cidade, bairro.bairro, tipo_logradouro.tipo_logradouro,
+                jsonb_agg(jsonb_build_object(
+                'id', comentario.id,
+                'nota', comentario.nota,
+                'conteudo', comentario.conteudo
+                )) AS comentarios
+                FROM cadastro_ponto_coleta
+                LEFT JOIN comentario ON cadastro_ponto_coleta.id = comentario.fk_ponto_coleta_id
+                LEFT JOIN endereco ON cadastro_ponto_coleta.fk_endereco_id = endereco.id
+                LEFT JOIN estado ON estado.id = endereco.fk_estado_id
+                LEFT JOIN cidade ON cidade.id = endereco.fk_cidade_id
+                LEFT JOIN bairro ON bairro.id = endereco.fk_bairro_id
+                LEFT JOIN tipo_logradouro ON tipo_logradouro.id = endereco.fk_tipo_logradouro_id
+                WHERE cadastro_ponto_coleta.id = :id
+                GROUP BY cadastro_ponto_coleta.nome, cadastro_ponto_coleta.id, cadastro_ponto_coleta.imagem,
+                    endereco.cep, endereco.latitude, endereco.longitude, endereco.logradouro,
+                    endereco.numero, endereco.complemento, estado.estado, cidade.cidade, bairro.bairro, tipo_logradouro.tipo_logradouro;";
+            $stmt = Database::prepare($sql);
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+             
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+
         public function update($id){
         
         }
---
+++ sql/mobile/getPontoColeta.php
@@ -0,0 +1,35 @@
+<?php
+    require_once './../entidades/pontoColeta/PontoColeta.php';
+
+    $resposta = array();
+
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+
+        $pontoColeta = PontoColeta::findPontoColetaById($id);
+
+        $resposta["id"] = $pontoColeta["id"];
+        $resposta["nome"] = $pontoColeta["nome"];
+        $resposta["imagem"] = $pontoColeta["imagem"];
+        $resposta["cep"] = $pontoColeta["cep"];
+        $resposta["latitude"] = $pontoColeta["latitude"];
+        $resposta["longitude"] = $pontoColeta["longitude"];
+        $resposta["logradouro"] = $pontoColeta["logradouro"];
+        $resposta["numero"] = $pontoColeta["numero"];
+        $resposta["complemento"] = $pontoColeta["complemento"];
+        $resposta["estado"] = $pontoColeta["estado"];
+        $resposta["cidade"] = $pontoColeta["cidade"];
+        $resposta["bairro"] = $pontoColeta["bairro"];
+        $resposta["tipo_logradouro"] = $pontoColeta["tipo_logradouro"];
+        $resposta["comentarios"] = $pontoColeta["comentarios"];
+        
+        $resposta["status"] = 1;
+    }else{
+        $resposta["status"] = 0;
+        $resposta["mensagem"] = "Faltam parâmetros";
+    }
+
+    // Converte a resposta para o formato JSON.
+    echo json_encode($resposta);
+
+?>
