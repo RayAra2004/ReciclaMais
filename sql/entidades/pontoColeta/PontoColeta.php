e12b15eaa07984fbdf6bb06354de473092d638a5
RayAra2004 <raynan.araujo@gmail.com>
2023-12-08 14:53:29 -0300

    correção descrição ponto

--- sql/entidades/pontoColeta/PontoColeta.php
+++ sql/entidades/pontoColeta/PontoColeta.php
@@ -110,26 +110,52 @@
         }
 
         public static function findPontoColetaById($id){
-            $sql = "SELECT cadastro_ponto_coleta.nome, cadastro_ponto_coleta.id, cadastro_ponto_coleta.imagem, cadastro_ponto_coleta.telefone,
-                endereco.cep, endereco.latitude, endereco.longitude, endereco.logradouro, endereco.numero,
-                endereco.complemento, estado.estado, cidade.cidade, bairro.bairro, tipo_logradouro.tipo_logradouro,
-                ROUND(avg(comentario.nota)) as nota, STRING_AGG(cmr.descricao, ', ') AS materiais_reciclados
-                FROM cadastro_ponto_coleta
-                LEFT JOIN comentario ON cadastro_ponto_coleta.id = comentario.fk_ponto_coleta_id
-                LEFT JOIN endereco ON cadastro_ponto_coleta.fk_endereco_id = endereco.id
-                LEFT JOIN estado ON estado.id = endereco.fk_estado_id
-                LEFT JOIN cidade ON cidade.id = endereco.fk_cidade_id
-                LEFT JOIN bairro ON bairro.id = endereco.fk_bairro_id
-                LEFT JOIN tipo_logradouro ON tipo_logradouro.id = endereco.fk_tipo_logradouro_id
-                INNER JOIN usuario ON usuario.id = comentario.fk_usuario_pessoa_fisica_fk_usuario_id
-                INNER JOIN recicla ON cadastro_ponto_coleta.id = recicla.fk_ponto_coleta_id
-                INNER JOIN categoria_de_materiais_reciclados cmr ON recicla.fk_categoria_de_materiais_reciclados_id = cmr.id
-                WHERE cadastro_ponto_coleta.id = :id
-                GROUP BY cadastro_ponto_coleta.nome, cadastro_ponto_coleta.id, cadastro_ponto_coleta.imagem,
-                endereco.cep, endereco.latitude, endereco.longitude, endereco.logradouro,
-                endereco.numero, endereco.complemento, estado.estado, cidade.cidade, bairro.bairro, tipo_logradouro.tipo_logradouro;";
+            $sql = "SELECT
+            cadastro_ponto_coleta.nome,
+            cadastro_ponto_coleta.id,
+            cadastro_ponto_coleta.imagem,
+            cadastro_ponto_coleta.telefone,
+            endereco.cep,
+            endereco.latitude,
+            endereco.longitude,
+            endereco.logradouro,
+            endereco.numero,
+            endereco.complemento,
+            estado.estado,
+            cidade.cidade,
+            bairro.bairro,
+            tipo_logradouro.tipo_logradouro,
+            ROUND(avg(comentario.nota)) as nota,
+            STRING_AGG(cmr.descricao, ', ') AS materiais_reciclados
+        FROM
+            cadastro_ponto_coleta
+        INNER JOIN endereco ON cadastro_ponto_coleta.fk_endereco_id = endereco.id
+        INNER JOIN estado ON estado.id = endereco.fk_estado_id
+        INNER JOIN cidade ON cidade.id = endereco.fk_cidade_id
+        INNER JOIN bairro ON bairro.id = endereco.fk_bairro_id
+        INNER JOIN tipo_logradouro ON tipo_logradouro.id = endereco.fk_tipo_logradouro_id
+        LEFT JOIN comentario ON cadastro_ponto_coleta.id = comentario.fk_ponto_coleta_id
+        LEFT JOIN usuario ON usuario.id = comentario.fk_usuario_pessoa_fisica_fk_usuario_id
+        LEFT JOIN recicla ON cadastro_ponto_coleta.id = recicla.fk_ponto_coleta_id
+        LEFT JOIN categoria_de_materiais_reciclados cmr ON recicla.fk_categoria_de_materiais_reciclados_id = cmr.id
+        WHERE
+            cadastro_ponto_coleta.id = :id
+        GROUP BY
+            cadastro_ponto_coleta.nome,
+            cadastro_ponto_coleta.id,
+            cadastro_ponto_coleta.imagem,
+            endereco.cep,
+            endereco.latitude,
+            endereco.longitude,
+            endereco.logradouro,
+            endereco.numero,
+            endereco.complemento,
+            estado.estado,
+            cidade.cidade,
+            bairro.bairro,
+            tipo_logradouro.tipo_logradouro;";
             $stmt = Database::prepare($sql);
-            $stmt->bindParam(":id", $id);
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
             $stmt->execute();
              
             return $stmt->fetch(PDO::FETCH_ASSOC);
