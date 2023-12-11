1438f972f3125e578eaff4b96cb2fdb3044eedb8
kaua <kaua271203@gmail.com>
2023-12-11 10:14:21 -0300

    ajustes em descrição ponto

--- sql/entidades/pontoColeta/PontoColeta.php
+++ sql/entidades/pontoColeta/PontoColeta.php
@@ -94,6 +94,36 @@
             $stmt->execute();
             return $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
+        
+        public static function findByIdPontosColetaMapa($id){
+            $sql = "SELECT cadastro_ponto_coleta.nome,avg(comentario.nota) as media, count(comentario.nota) as quantos, sum(comentario.nota) as soma, cadastro_ponto_coleta.id, cadastro_ponto_coleta.imagem,endereco.cep, endereco.latitude, endereco.longitude, endereco.logradouro, endereco.numero, endereco.complemento, estado.estado, cidade.cidade, bairro.bairro, tipo_logradouro.tipo_logradouro,   jsonb_agg(jsonb_build_object(
+                'id', categoria_de_materiais_reciclados.descricao
+              )) AS materiais_reciclados, cadastro_ponto_coleta.telefone
+                            from comentario
+                            left JOIN cadastro_ponto_coleta
+                            ON cadastro_ponto_coleta.id = comentario.fk_ponto_coleta_id
+                            left JOIN recicla
+                            ON cadastro_ponto_coleta.id = recicla.fk_ponto_coleta_id
+                            left join categoria_de_materiais_reciclados
+                            on categoria_de_materiais_reciclados.id = recicla.fk_categoria_de_materiais_reciclados_id
+                            left join endereco
+                            on cadastro_ponto_coleta.fk_endereco_id = endereco.id
+                            left JOIN estado
+                            ON estado.id = endereco.fk_estado_id
+                            left JOIN cidade
+                            ON cidade.id = endereco.fk_cidade_id
+                            left JOIN bairro
+                            ON bairro.id = endereco.fk_bairro_id
+                            left JOIN tipo_logradouro
+                            ON tipo_logradouro.id = endereco.fk_tipo_logradouro_id
+                            WHERE cadastro_ponto_coleta.id = :id
+                            group by cadastro_ponto_coleta.nome, cadastro_ponto_coleta.id, cadastro_ponto_coleta.imagem,endereco.cep, endereco.latitude, endereco.longitude, endereco.logradouro, endereco.numero, endereco.complemento, estado.estado, cidade.cidade, bairro.bairro, tipo_logradouro.tipo_logradouro, recicla.fk_ponto_coleta_id
+                            order by media DESC;";
+            $stmt = Database::prepare($sql);
+            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
 
         public static function findPontosColetaPaginado($limit, $offset, $latitude, $longitude){
             $sql = "WITH DistanceCTE AS (
